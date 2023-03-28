5fe3de09e9e8b01bf0adf723797bb8877fed489b
GamingVideos <78279767+WatchGamingVideos@users.noreply.github.com>
2023-03-28 16:34:13 -0400

    Create event.js

---
+++ event.js
@@ -0,0 +1,2 @@
+<script> (function(a,b,c,d,e,f,g,h,i){a[e]||(i=a[e]=function(){(a[e].q=a[e].q||[]).push(arguments)},i.l=1*new Date,i.o=f, g=b.createElement(c),h=b.getElementsByTagName(c)[0],g.async=1,g.src=d,g.setAttribute("n",e),h.parentNode.insertBefore(g,h) )})(window,document,"script","https://widgets.media.sportradar.com/uscommon/widgetloader","SIR", { language: 'en_us' }); SIR('addWidget', '#sr-widget', 'us.common.matchList', {sportId: 3, onItemClick: function(type, obj){alert('matchId: ' + obj.matchId);}}); </script>
+<div id="sr-widget"></div>
